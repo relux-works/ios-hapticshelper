79261c8a54666447495242e0f8e48e1e1c29ffda
alexis <alexis.ag85@gmail.com>
2022-05-05 21:04:23 +0300

    setup

--- Sources/HapticEffectService.swift
+++ Sources/HapticEffectService.swift
@@ -1,34 +1,32 @@
-import AVFoundation
 import UIKit
 
-protocol IHapticEffectService {
-
+public protocol IHapticEffectService {
     func doImpact(style: UIImpactFeedbackGenerator.FeedbackStyle, intensity: Float)
     func doSelectionImpact()
     func doNotificationImpact(for feedbackType: UINotificationFeedbackGenerator.FeedbackType)
 }
 
-class HapticEffectService: IHapticEffectService {
+public class HapticEffectService: IHapticEffectService {
 
     private var impactFeedbackGenerator: UIImpactFeedbackGenerator?
     private var selectionFeedbackGenerator: UISelectionFeedbackGenerator = .init()
     private var notificationFeedbackGenerator: UINotificationFeedbackGenerator = .init()
     private var impactFeedbackGeneratorLastUsedStyle: UIImpactFeedbackGenerator.FeedbackStyle?
 
-    init() {
+    public init() {
         selectionFeedbackGenerator.prepare()
     }
 
-    func doImpact(style: UIImpactFeedbackGenerator.FeedbackStyle, intensity: Float) {
+    public func doImpact(style: UIImpactFeedbackGenerator.FeedbackStyle, intensity: Float) {
         impactFeedbackGenerator = .init(style: style)
         impactFeedbackGenerator?.impactOccurred(intensity: CGFloat(intensity))
     }
 
-    func doSelectionImpact() {
+    public func doSelectionImpact() {
         selectionFeedbackGenerator.selectionChanged()
     }
 
-    func doNotificationImpact(for feedbackType: UINotificationFeedbackGenerator.FeedbackType) {
+    public func doNotificationImpact(for feedbackType: UINotificationFeedbackGenerator.FeedbackType) {
         notificationFeedbackGenerator.notificationOccurred(feedbackType)
     }
 }
